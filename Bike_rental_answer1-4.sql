03c57fe72565890ec16b81805d1990154d19700d
Obsone <jsdcone49@gmail.com>
2024-09-21 10:40:42 +0800

    answered Qs 1-4

---
+++ Bike_rental_answer1-4.sql
@@ -0,0 +1,49 @@
+-- 1
+SELECT category, count(model) AS number_of_bikes 
+FROM bike
+GROUP BY category
+HAVING count(model) > 2;
+
+
+-- 2
+SELECT c.name, count(m.id) AS membership_count
+FROM customer AS c
+LEFT JOIN membership AS m ON c.id = m.customer_id
+GROUP BY c.name
+ORDER BY count(0) DESC;
+TO identify the LEFT AND RIGHT TABLES:
+Look AT the TABLE IN the FROM clause: this IS your LEFT table.
+Look AT the TABLE IN the JOIN clause: this IS your RIGHT table.
+
+-- 3 
+SELECT
+    id, category,
+    price_per_hour AS old_price_per_hour,
+        CASE
+            WHEN category = 'electric' THEN round(price_per_hour - (price_per_hour*0.1), 2)
+            WHEN category = 'mountain bike' THEN round(price_per_hour -(price_per_hour*0.2), 2)
+            ELSE round(price_per_hour - (price_per_hour*0.5), 2)
+        END AS new_price_per_hour,
+    price_per_day AS old_price_per_day,
+        CASE
+            WHEN category = 'electric' THEN round(price_per_day - (price_per_day*0.2), 2)
+            WHEN category = 'mountain bike' THEN round (price_per_day - (price_per_day*0.5), 2)
+            ELSE round(price_per_day - (price_per_day*0.5), 2)
+        END AS new_price_per_day
+FROM bike;
+
+-- 4 
+SELECT 
+    category,
+    COUNT(*) FILTER (WHERE status = 'available') AS available_bikes_count,
+    COUNT(*) FILTER (WHERE status = 'rented') AS rented_bikes_count
+FROM bike
+GROUP BY category;
+-- alternative
+SELECT 
+    category,
+    COUNT(CASE WHEN status = 'available' THEN 1 END) AS available_bikes_count,
+    COUNT(CASE WHEN status = 'rented' THEN 1 END) AS rented_bikes_count
+FROM bike
+GROUP BY category;
+
